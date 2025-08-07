d60b1436bf0d69c594e1ca2d99f4332dc0b21392
Andrey D. Viana <andreydjason@gmail.com>
2025-08-07 08:48:41 -0300

    incluindo api prisma

---
+++ app/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  // para evitar criar múltiplas instâncias no hot reload do dev
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+export default prisma;
